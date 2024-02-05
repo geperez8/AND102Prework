5b3a4c2e1f9c40e56cdcbdef456aa8d6e2fbe0db
Zyanik <106106326+geperez8@users.noreply.github.com>
2024-02-04 19:10:52 -0600

    Initialization

---
+++ app/src/main/java/com/example/and102_prework/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.and102_prework
+
+import android.os.Bundle
+import android.util.Log
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Button
+import android.widget.Toast
+import com.example.and102_prework.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val button = findViewById<Button>(R.id.clickButton)
+
+        button.setOnClickListener {
+            Toast.makeText(this, "Hello to you too!", Toast.LENGTH_LONG).show()
+        }
+    }
+}
